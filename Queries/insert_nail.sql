525c93d41fb0c734ed758a286bc9bc664295bf2e
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-04-20 23:13:20 -0400

    update

---
+++ Queries/insert_nail.sql
@@ -0,0 +1,41 @@
+-- SQLite
+INSERT INTO Nail (id, name, description, images, upgrade_cost, damage)
+VALUES (
+'aa74802b-28e5-4f46-a526-c466f5b359df',
+'Old Nail',
+'A traditional weapon of Hallownest. Its blade is blunt with age and wear.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745083196/Nail_1_Old_Nail_c1yom6.webp"]',
+'Default weapon',
+5);
+INSERT INTO Nail (id, name, description, images, upgrade_cost, damage)
+VALUES (
+'0b795b55-1523-48d9-b662-964b0ebb5842',
+'Sharpened Nail',
+'A traditional weapon of Hallownest, restored to lethal form.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745083206/Nail_2_Sharpened_Nail_senjze.webp"]',
+'250 Geo',
+9);
+INSERT INTO Nail (id, name, description, images, upgrade_cost, damage)
+VALUES (
+'96433abb-6464-4422-90f6-bf06d4f570ef',
+'Channelled Nail',
+'A cleft weapon of Hallownest. The blade is exquisitly balanced.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745083201/Nail_3_Channelled_Nail_mgw4pv.webp"]',
+'800 Geo + 1 Pale Ore',
+13);
+INSERT INTO Nail (id, name, description, images, upgrade_cost, damage)
+VALUES (
+'911a08a8-7c0e-4e16-9d7f-f1996fba9e32',
+'Coiled Nail',
+'A powerful weapon of Hallownest, refined beyond all others.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745083211/Nail_4_Coiled_Nail_gdzzbd.webp"]',
+'2000 Geo + 2 Pale Ore',
+17);
+INSERT INTO Nail (id, name, description, images, upgrade_cost, damage)
+VALUES (
+'5bb3aeb7-4129-4b9b-a661-b3ff69cd3a55',
+'Pure Nail',
+'The ultimate weapon of Hallownest. Crafted to perfection, this ancient nail reveals its true form.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745083216/Nail_5_Pure_Nail_r1yf7e.webp"]',
+'4000 Geo + 3 Pale Ore',
+21);
