f7ae0ca9910d96ad0dc860ca15431adb81fe8e97
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-04-23 16:52:02 -0400

    Update insert_spells_and_abilities.sql

--- Queries/insert_spells_and_abilities.sql
+++ Queries/insert_spells_and_abilities.sql
@@ -1,267 +1,264 @@
 -- SQLite
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'64720673-4508-43ab-9825-6045e6747b51',
+'Crystal Heart',
+'The energy core of an old mining golem, fashioned around a potent crystal. The crystal s energy can be channeled to launch the bearer forward at dangerous speeds.',
 '',
-'',
-'',
-'',
-'',
-'',
-'[""]',
-'',
-'',
-'',
+'Hold SUPER DASH while on the ground or clinging to a wall to concentrate the force.',
+'Release the button to blast forwards and fly through the air.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084860/Inventory_mvh2sk.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084868/Prompt_twsdoa.webp"]',
+'Hold SUPER DASH while on the ground or clinging to a wall to concentrate force. Release the button at full charge to blast forwards and fly through the air. The Knight continues moving forward until interrupted by damage or the environment, by pressing JUMP or by tapping SUPER DASH again.
+
+The Super Dash deals 10 damage on contact with an enemy, and any enemy killed by this hit does not damage the Knight or stop their movement.',
+'Found in an old mining golem at the end of a platforming segment in Crystal Peak. Requires Mantis Claw and Mothwing Cloak.',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
-'',
-'',
-'',
-'',
-'',
-'',
-'[""]',
-'',
-'',
-'',
+'71fe0163-3067-43ac-94da-8a9765f3f23f',
+'Dream Nail',
+'Allows the wielder to cut through the veil between dreams and waking. Can be used to reveal hidden dreams or open gateways.',
+'The Dream Nail can collect Essence, remnants of wishes and dreams.',
+'Hold DREAM NAIL to charge and slash with the nail.',
+'Cut through the veil between dreams and waking.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084888/Inventory_1_kvn7az.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084895/Inventory_2_mfchhc.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084902/Prompt_1_qjl9w3.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084909/Prompt_2_krx1jr.webp"]',
+'Hold down DREAM NAIL to charge and swing the Dream Nail.',
+'The Dream Nail is acquired in the Resting Grounds by inspecting the Dreamers Memorial. Doing so sends the Knight into a dream where they receive the Dream Nail from a Moth.',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
-'',
-'',
-'',
-'',
-'',
-'',
-'[""]',
-'',
-'',
-'',
+'100afe06-4a8a-47fb-8f8d-f9f380a2fb5a',
+'Dreamgate',
+'Allows the wielder to travel instantly through dream.',
+'Some areas may lack a strong connection to any dream, disallowing use of the Dreamgate.',
+'Hold DOWN while using the Dream Nail to set a Dreamgate.',
+'Hold UP while using the Dream Nail to travel to the Dreamgate.',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084874/Inventory_pnhnz1.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084881/Prompt_tzofmv.webp"]',
+'Allows the Knight to place a Dreamgate at a location, then warp to it from anywhere. Only one Dreamgate can be placed at any time, and placing a new gate removes the previous one. Warping consumes 1 Essence. A Dreamgate can be warped to indefinitely, as long as the Knight has enough Essence. Setting a Dreamgate takes ~3.5 seconds and warping to one takes ~2.75 seconds. The Dream Wielder Charm reduces both of these times by ~0.5 seconds.
+
+Once set, an icon appears on the map, showing the location of the Dreamgate.
+Limitations
+The Dream Nail cannot be used to place or warp to a gate while moving or while inside the Ancestral Mound, either functioning Tram, the rooms in the City of Tears containing lifts to the Forgotten Crossroads and the Resting Grounds, Beast s Den, the White Palace, the Lifeblood Chamber, the Cast-Off Shell, the Dreamers  dreams, the Birthplace memory, or the rooms containing the White Lady, the Hollow Knight, and the Radiance.
+
+Additionally, the Dream Nail cannot be used to place a gate in any interior location (that is, any location not on the Map, including dreams but excluding the room containing Seer), the Pleasure House and its lift shaft, all boss arenas that cannot be exited freely, most arenas that are closed by gates, all Husk Guard, Great Husk Sentry, Entombed Husk, and Stalking Devout spawn locations, various locations that can become inaccessible at any time, and the rooms containing Unn and Hive Knight.
+
+Additionally, the Dream Nail cannot be used to warp to a gate while inside the room containing Soul Master.
+',
+'The Dreamgate is acquired in the Resting Grounds by talking to Seer after having acquired 900 Essence.',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'e7440a35-66dd-4a28-a0d3-f790c1f61b6e',
+'Isma s Tear',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084916/Inventory_zjpeiu.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084923/Prompt_azj54t.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'e7c4eb07-c3c4-4657-af82-deee22062c34',
+'Mantis Claw',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084930/Inventory_co6kdf.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084937/Prompt_c5vov2.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'daea6066-845a-4572-97c9-1c59892a6833',
+'Monarch Wings',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084944/Inventory_cqrkeh.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084951/Prompt_cew1qp.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'eb258266-3356-480d-9022-ca1d3e963409',
+'Mothwing Cloak',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084958/Inventory_bkcrcu.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084965/Prompt_drupfg.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'4ee03689-178f-4112-b7f1-de6db411164a',
+'Shade Cloak',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084972/Inventory_jksz54.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084979/Prompt_tgimll.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Ability',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'f1e5ef7a-7d50-44db-bef1-f7383a8fb754',
+'Cyclone Slash',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084707/Inventory_sdvmbd.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084714/Prompt_fngoc3.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Nail Art',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'31019e04-b989-4bc4-8b55-407bf509053a',
+'Dash Slash',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084721/Inventory_v1rkac.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084728/Prompt_jyxmlh.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Nail Art',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'20311ae0-a7db-4d41-bf49-4a8e7290b728',
+'Great Slash',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084735/Inventory_e8nqiq.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084742/Prompt_xxvalv.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Nail Art',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'44aedb4f-f800-4a1d-90f1-e7458e39a96b',
+'Abyss Shriek',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084749/Inventory_pz2otx.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084756/Prompt_lhtq3d.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'829d445d-d1aa-405e-88fd-fcf5b8d0a190',
+'Descending Dark',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084763/Inventory_mcmmuo.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084770/Prompt_eblupl.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'01b19d91-f4ce-4e94-9f71-473ae5c9b1a9',
+'Desolate Dive',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084777/Inventory_spcfau.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084784/Prompt_jhoy7d.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'5888af30-0d2a-4c23-b476-14f1f7d5a94f',
+'Focus',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084791/Inventory_jiuqs8.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084798/Prompt_ri2iea.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'c6969d31-76e1-42e7-a131-66b1ab649562',
+'Shade Soul',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084819/Inventory_ae1vc5.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084826/Prompt_cgyrva.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'fced9cd1-2234-4e31-80ed-3d8a3962c996',
+'Vengeful Spirit',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084833/Inventory_fpwjsd.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084839/Prompt_dumupd.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
 INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
 VALUES (
+'12789bb9-af42-4543-ba60-16c6ec3b0821',
+'Howling Wraiths',
 '',
 '',
 '',
 '',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084805/Inventory_as2xxe.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745084812/Prompt_glavn7.webp"]',
 '',
 '',
-'[""]',
-'',
-'',
-'',
-''
-);
-INSERT INTO SpellsAndAbilities (id, name, inventory_description_1, inventory_description_2, prompt_description_1, prompt_description_2, images, function, how_to_acquire, type, modifiers)
-VALUES (
-'',
-'',
-'',
-'',
-'',
-'',
-'[""]',
-'',
-'',
-'',
+'Spell',
 ''
 );
