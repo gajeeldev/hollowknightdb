2fb69a12dd624ef2e5493e286672c371254ded87
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-04-24 20:05:10 -0400

    Update insert_items.sql

--- Queries/insert_items.sql
+++ Queries/insert_items.sql
@@ -1,23 +1,11 @@
 -- SQLite
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'c702d452-b26a-420c-8993-6d745cf8ccb6',
+'Arcane Egg',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081684/Arcane_Egg_bhfmjk.webp"]',
 '[""]',
 '',
 '',
@@ -25,23 +13,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'9da3f6da-f085-41c7-ae09-104bc2dd6fd6',
+'City Crest',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081689/City_Crest_ffzbxn.webp"]',
 '[""]',
 '',
 '',
@@ -49,23 +25,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'57989e0f-73bf-455e-b413-c11996a412df',
+'Collector s Map',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081693/Collector_s_Map_kuozms.webp"]',
 '[""]',
 '',
 '',
@@ -73,23 +37,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'a5ec4500-a2e0-47c3-af14-108da2d1244e',
+'Delicate Flower',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081698/Delicate_Flower_1_ndwweo.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081702/Delicate_Flower_2_ektcjt.webp"]',
 '[""]',
 '',
 '',
@@ -97,23 +49,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'bf2e0951-5b59-449c-a67e-b168d49d4198',
+'Elegant Key',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081707/Elegant_Key_m9ztki.webp"]',
 '[""]',
 '',
 '',
@@ -121,23 +61,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'acda456f-c4e6-4331-a045-1d1d3d81d106',
+'Godtuner',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081711/Godtuner_1_njuofy.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081716/Godtuner_2_qgex2r.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081720/Godtuner_3_qcpahe.webp"]',
 '[""]',
 '',
 '',
@@ -145,23 +73,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'bf3eaa1f-385a-4621-8c81-cfbd8850288f',
+'Hallownest Seal',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081725/Hallownest_Seal_us9ord.webp"]',
 '[""]',
 '',
 '',
@@ -169,23 +85,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'078882d2-a9c8-4001-8dd6-a90598691d3a',
+'Hunter s Journal',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081729/Hunter_s_journal_qfugp8.webp"]',
 '[""]',
 '',
 '',
@@ -193,23 +97,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'fcd64741-41b2-4179-a1b7-a21cb956588f',
+'Hunter s Mark',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081734/Hunter_s_Mark_1_oawyx3.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081738/Hunter_s_Mark_2_yd6aof.webp"]',
 '[""]',
 '',
 '',
@@ -217,23 +109,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'ac173145-9297-416d-8dd0-1cb26110276a',
+'King s Brand',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081743/King_s_Brand_1_udytiv.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081743/King_s_Brand_1_udytiv.webp"]',
 '[""]',
 '',
 '',
@@ -241,23 +121,11 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'5c555620-a6c6-4cd8-99e6-92e721eca931',
+'King s Idol',
 '',
 '',
-'',
-'',
-'[""]',
-'[""]',
-'',
-'',
-''
-);
-INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
-VALUES (
-'',
-'',
-'',
-'',
-'[""]',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081752/King_s_Idol_hciki0.webp"]',
 '[""]',
 '',
 '',
@@ -265,11 +133,143 @@ VALUES (
 );
 INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
 VALUES (
+'06c99e22-1203-4018-9ff5-f29cbe50efd7',
+'Love Key',
 '',
 '',
-'',
-'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081757/Love_Key_nyqwu7.webp"]',
 '[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'df49e47d-6c55-4cf6-801a-ab3a27ab3c9e',
+'Lumafly Lantern',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081761/Lumafly_Lantern_nwmgyd.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'76e9460b-0cfa-474f-b1a1-1ef1f1ed9f09',
+'Map and Quill',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081824/Map_and_Quill_demqwa.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'791fae71-cc71-4b1c-84b3-591bb33e0b98',
+'Mask Shard',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081937/Mask_Shard_1_tuvsc0.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081942/Mask_Shard_2_dzb5qg.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'4a6c0901-3406-43b7-827a-2faf822d5530',
+'Pale Ore',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081946/Pale_Ore_k6fe49.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'9e04ec7d-6fd6-4d85-bdfa-2d591fcb3fb7',
+'Rancid Egg',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081952/Rancid_Egg_iabqhu.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'47a12397-1d01-4b2a-a8e0-a9b84ffabc75',
+'Salubra s Blessing',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081956/Salubra_s_Blessing_rqm0lk.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'c7800b3c-2316-40bb-baee-231d5d4cf1f9',
+'Shopkeeper s Key',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081961/Shopkeeper_s_Key_dmvpnm.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'71151d27-a1cf-485b-9aa0-11b1d0e0887f',
+'Simple Key',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081966/Simple_Key_h6oc6m.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'0310d18a-8695-433a-a0ab-5e2110e3bd95',
+'Tram Pass',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081971/Tram_Pass_heoire.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'9f4aa8d3-fa3a-4ee0-8210-4194195e712d',
+'Vessel Fragment',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081976/Vessel_Fragment_1_vzgbqz.webp","https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081981/Vessel_Fragment_2_m8rtz6.webp"]',
+'[""]',
+'',
+'',
+''
+);
+INSERT INTO Items (id, name, description_1, description_2, images, effects, type, use, how_to_acquire)
+VALUES (
+'c8da4fcd-d85e-40d8-b34b-9265af2183f1',
+'Wanderer s Journal',
+'',
+'',
+'["https://res.cloudinary.com/dqna4zqh5/image/upload/v1745081986/Wanderer_s_Journal_oquwxi.webp"]',
 '[""]',
 '',
 '',
