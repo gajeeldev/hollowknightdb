0f88154eaa3796455b9e1587a31e8662eafb33b5
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-04-29 21:37:53 -0400

    update

---
+++ Queries/create indexes.sql
@@ -0,0 +1,10 @@
+-- SQLite
+CREATE INDEX idx_nail_id ON Nail (id);
+CREATE INDEX idx_achievement_id ON Achievements (id);
+CREATE INDEX idx_area_id ON Areas (id);
+CREATE INDEX idx_charm_id ON Charms (id);
+CREATE INDEX idx_enemy_id ON Enemies (id);
+CREATE INDEX idx_npc_id ON Npcs (id);
+CREATE INDEX idx_spell_and_ability_id ON SpellsAndAbilities (id);
+CREATE INDEX idx_item_id ON Items (id);
+CREATE INDEX idx_boss_id ON Bosses (id);
